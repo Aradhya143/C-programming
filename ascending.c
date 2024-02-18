778595aa48798fe63d132830ae4f7594deb3c7f8
Aradhya Rastogi <142193262+Aradhya143@users.noreply.github.com>
2024-02-18 23:27:52 +0530

    Add files via upload
    Write a c program to insert elements in an array and sort the array in ascending order.

---
+++ ascending.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void ascending(int arr[]){
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5 ;j++){
+            if(arr[i]<arr[j]){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+      for (int k=0;k<5;k++){
+        printf("%d ",arr[k]);
+       }
+    return;
+}
+int main(){
+    int arr[5] = {5,6,3,8,4};
+    ascending(arr);
+    return 0;
+}
